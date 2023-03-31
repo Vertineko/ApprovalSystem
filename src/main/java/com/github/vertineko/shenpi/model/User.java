33a225f5e475c481c8677e582ae4f3cf83ebd2ff
Vertineko <predator@apex.com>
2023-03-31 17:00:32 +0800

    Initial commit

---
+++ src/main/java/com/github/vertineko/shenpi/model/User.java
@@ -0,0 +1,49 @@
+package com.github.vertineko.shenpi.model;
+
+public class User {
+    private int id;
+    private String name;
+    private String telephone;
+    private String account;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
